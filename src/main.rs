cf0b61504cd92e40f15fb3efee0953435cb5c584
PsypherPunk <psypherpunk@gmail.com>
2023-05-17 22:28:23 +0100

    feat: move to async retrieval
    - retrieve secrets using `async` to avoid consecutive waits;
    - build output and write once to avoid write penalties.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
 use std::env;
+use std::io::{self, Write};
 
 use aws_sdk_secretsmanager::Client;
-use eyre::{eyre, Result};
+use eyre::Result;
+use futures::future::try_join_all;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = aws_config::from_env().load().await;
     let client = Client::new(&config);
 
-    for (key, value) in env::vars() {
-        if key.ends_with("_SECRET_ARN") {
-            let secret_value = client.get_secret_value().secret_id(&value).send().await?;
-            let secret_string = secret_value
-                .secret_string()
-                .ok_or_else(|| eyre!("could not read SecretString"))?;
+    let secret_envs = env::vars()
+        .into_iter()
+        .filter(|(key, _)| key.ends_with("_SECRET_ARN"))
+        .collect::<Vec<_>>();
 
-            println!(
-                "export {}='{}'",
-                key.trim_end_matches("_SECRET_ARN"),
-                secret_string,
-            );
-        }
-    }
+    let secret_arns = secret_envs
+        .iter()
+        .map(|(_, value)| client.get_secret_value().secret_id(value).send())
+        .collect::<Vec<_>>();
+
+    let secret_values = try_join_all(secret_arns).await?;
+
+    let secret_strings = secret_values
+        .iter()
+        .zip(secret_envs)
+        .filter_map(|(secret_value, (key, _))| {
+            let secret_string = secret_value.secret_string()?;
+
+            Some(format!("{}='{}'", key, secret_string))
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    io::stdout().write_all(secret_strings.as_bytes())?;
 
     Ok(())
 }
